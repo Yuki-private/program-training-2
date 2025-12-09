56b6b49d00d08bc2ba5adabef48b46dd8b08056a
Yuki Ikuyama <j3308cfq@sit.ac.jp>
2025-12-09 16:05:03 +0900

    add timer

--- train.py
+++ train.py
@@ -45,7 +45,7 @@ learning_rate = 1e-3
 optimizer = torch.optim.SGD(model.parameters(),lr=learning_rate)
 
 
-n_epochs = 5
+n_epochs = 20
 
 train_loss_log =[]
 val_loss_log = []
@@ -57,19 +57,25 @@ for epoch in range(n_epochs):
     time_start = time.time()
     train_loss = models.train(model,dataloader_train,loss_fn,optimizer)
     time_end = time.time()    
-    print(f' training loss: {train_loss} ({time_end-time_start:.3f}s)')
+    print(f'   training loss: {train_loss:.3f} ({time_end-time_start:.3f}s)')
     train_loss_log.append(train_loss)
 
+    time_start = time.time()
     val_loss = models.test(model,dataloader_test,loss_fn)
-    print(f'   validation loss: {val_loss}')
+    time_end = time.time()
+    print(f'   validation loss: {val_loss:.3f} ({time_end-time_start:.3f}s)')
     val_loss_log.append(val_loss)
 
+    time_start = time.time()
     train_acc = models.test_accuracy(model,dataloader_train)
-    print(f'   training accuracy{train_acc*100:.3f}%')
+    time_end = time.time()
+    print(f'   training accuracy{train_acc*100:.3f}% ({time_end-time_start:.3f}s)')
     train_acc_log.append(train_acc)
 
+    time_start = time.time()
     val_acc = models.test_accuracy(model,dataloader_test)
-    print(f"   validiation accuracy: {val_acc*100:.3f}%")
+    time_end = time.time()
+    print(f"   validiation accuracy: {val_acc*100:.3f}% ({time_end-time_start:.3f}s)")
     val_acc_log.append(val_acc)
 
 
